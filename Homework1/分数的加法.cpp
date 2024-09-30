bb66dbcc764fbe91acc559e5c766728cd7853b0b
Chengyang He <3251713682@qq.com>
2024-09-30 23:54:30 +0800

    1

---
+++ Homework1/分数的加法.cpp
@@ -0,0 +1,22 @@
+//
+// Created by 何承洋 on 24-9-30.
+//
+#include <iostream>
+using namespace std;
+int
+
+
+
+
+
+int main() {
+    int b1,a1;
+    int b2,a2;
+    cin>>b1>>a1>>b2>>a2;
+    int c=0;
+
+
+
+
+    return 0;
+}
