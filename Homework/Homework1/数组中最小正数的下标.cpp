1eb74dc526b4245795874a7ffd025b9416884f44
Chengyang He <3251713682@qq.com>
2024-10-02 18:33:02 +0800

    1

--- Homework/Homework1/数组中最小正数的下标.cpp
+++ Homework/Homework1/数组中最小正数的下标.cpp
@@ -3,13 +3,23 @@
 //
 #include <iostream>
 using namespace std;
-
-
-
-int main() {
-
-
-
-
-    return 0;
+int FindiMinPos(int arr[],int size) {
+    int temp=-1;
+    for(int i=0;i<size;i++) {
+        if(arr[i]>0){
+            if(temp==-1||arr[i]<arr[temp]){
+                temp=i;
+            }
+        }
+    }return temp;
 }
+int main()
+    {
+    int n1,n2;
+    cin>>n1>>n2;
+    int a[n1],b[n2];
+    for(int i=0;i<n1;i++) {cin>>a[i];}
+    for(int i=0;i<n2;i++) {cin>>b[i];}
+    cout<<FindiMinPos(a,n1)<<" "<<FindiMinPos(b,n2)<<endl;
+    return 0;
+   }
