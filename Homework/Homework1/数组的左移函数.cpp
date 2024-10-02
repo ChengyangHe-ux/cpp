fba000b1cede46475656759a064246baf2526205
Chengyang He <3251713682@qq.com>
2024-10-02 16:59:13 +0800

    1

--- Homework/Homework1/数组的左移函数.cpp
+++ Homework/Homework1/数组的左移函数.cpp
@@ -1,3 +1,23 @@
 //
 // Created by 何承洋 on 24-10-2.
 //
+#include <iostream>
+using namespace std;
+void MoveLeft(int arr[],int size) {
+    int temp=arr[0];
+    for (int i=0; i<size-1; i++) {
+        arr[i]=arr[i+1];
+    }
+    arr[size-1]=temp;
+}
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0; i<n; i++) {cin>>arr[i];}
+    int k;
+    cin>>k;
+    for(int i=0;i<k;i++) {MoveLeft(arr,n);}
+    for(int i=0;i<n;i++) {cout<<arr[i]<<" ";}
+    return 0;
+}
