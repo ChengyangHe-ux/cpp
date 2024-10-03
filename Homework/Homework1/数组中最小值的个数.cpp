c6bb93aa851721499b79d915cde2ad4a6b8d1d41
Chengyang He <3251713682@qq.com>
2024-10-03 21:36:04 +0800

    1

--- Homework/Homework1/数组中最小值的个数.cpp
+++ Homework/Homework1/数组中最小值的个数.cpp
@@ -1,3 +1,30 @@
 //
 // Created by 何承洋 on 24-10-3.
 //
+#include <iostream>
+using namespace std;
+int FindMinCount(int arr[],int size){
+    int min=arr[0];
+    for(int i=0;i<size;i++) {
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    int count=0;
+    for(int i=0;i<size;i++){
+        if(arr[i]==min){
+            count++;
+        }
+    }    return count;
+}
+int main(){
+    int n1,n2;
+    cin>>n1>>n2;
+    int a[10000],b[10000];
+    for(int i=0;i<n1;i++){cin>>a[i];}
+    for(int i=0;i<n2;i++){cin>>b[i];}
+    int x1=FindMinCount(a,n1);
+    int x2=FindMinCount(b,n2);
+    cout<<x1<<" "<<x2<<endl;
+    return 0;
+}
