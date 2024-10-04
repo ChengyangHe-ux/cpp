cfd98f31746223030b90445b90a39b57ebbee3ef
Chengyang He <3251713682@qq.com>
2024-10-04 11:30:25 +0800

    1

--- Homework/Homework1/数组中所有极大值.cpp
+++ Homework/Homework1/数组中所有极大值.cpp
@@ -1,53 +1,25 @@
 //
 // Created by 何承洋 on 24-10-3.
 //
-#include  <iostream>
-#include <climits>
-using namespace  std;
-int FindiSecondMax(int arr[],int size) {
-    if(size<2) {
-        return -1;
-    }
-    int firstmax=INT_MIN;
-    int secondmax=INT_MIN;
-    int firstindex=-1;
-    int secondindex=-1;
-    for(int i=0;i<size;i++){
-        if(arr[i]>firstmax){
-            secondmax = firstmax;
-            secondindex = firstindex;
-            firstmax = arr[i];
-            firstindex = i;
-        }else if(arr[i]>secondmax && arr[i]<firstmax){
-            secondmax = arr[i];
-            secondindex = i;
-        }else if(arr[i]==firstmax){
-            secondmax= arr[i];
+#include <iostream>
+using namespace std;
+void FindMaxs(int arr[],int size) {
+    for( int i=0;i<size;i++) {
+        if(i==0&&arr[i]>arr[i+1]){cout<<arr[i]<<" ";}
+        else if(arr[i]>arr[i-1]&&arr[i]>arr[i+1]) {
+            cout<<arr[i]<<" ";
         }
+        else if(i==size-1){cout<<arr[i]<<" ";}
     }
-    if(firstmax==secondmax) {
-        int arr2[size]={0};
-        int e=-1;
-        for(int i=0;i<size;i++) {
-            int d=arr[i];
-            if(arr[i]==firstmax) {
-                e++;
-                arr2[e] = i;
-            }
-        }
-        secondindex=arr2[1];
-    }
-    return secondindex;
-
 }
-
-
 int main() {
     int n1,n2;
     cin>>n1>>n2;
-    int a[n1]={0},b[n2]={0};
+    int a[n1],b[n2];
     for(int i=0;i<n1;i++) {cin>>a[i];}
     for(int i=0;i<n2;i++) {cin>>b[i];}
-    cout<<FindiSecondMax(a,n1)<<" "<<FindiSecondMax(b,n2)<<endl;
+    FindMaxs(a,n1);
+    cout<<endl;
+    FindMaxs(b,n2);
     return 0;
 }
